85fe79cfa9c231327ffaf7a56a6f2c1d60b7d780
Paparsifal <paparsifal@gmail.com>
2021-04-18 15:12:08 +0200

    tirrages normaux

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,62 @@
 <template>
-  <h1>Hello World</h1>
+  <h1 class="text-4xl text-center font-serif p-5">Mythic Game Master Tool</h1>
+  <div class="flex flex-wrap">
+    <boutton
+      v-for="bt in btList"
+      :key="bt.id"
+      :bt-name="bt.name"
+      @click="tirage(bt.proba)"
+    />
+  </div>
+  <div
+    class="text-xl text-center bg-gray-300 p-5"
+    :class="{
+      'bg-green-300': reponse == 'OUI',
+      'bg-red-300': reponse == 'NON',
+    }"
+  >
+    {{ reponse }}
+  </div>
+  <div class="font-mono font-thin p-2 bg-yellow-50">
+    <p v-for="(log, idx) in logTirage" :key="idx">{{ log }}</p>
+  </div>
 </template>
 
 <script>
+import Boutton from "./components/Boutton.vue";
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      btList: [
+        { proba: 99, id: "IM", name: "Impossible" },
+        { proba: 85, id: "QI", name: "Quasi Impossible" },
+        { proba: 70, id: "I", name: "Improbable" },
+        { proba: 60, id: "PP", name: "Peu probable" },
+        { proba: 50, id: "50", name: "50/50" },
+        { proba: 40, id: "AP", name: "Assez probable" },
+        { proba: 30, id: "P", name: "Probable" },
+        { proba: 20, id: "QS", name: "Quasi sûr" },
+        { proba: 10, id: "S", name: "Sûr" },
+      ],
+      reponse: "",
+      logTirage: [],
+    };
+  },
+  components: { Boutton },
+  methods: {
+    tirage(proba) {
+      this.reponse = "";
+      let tirage = Math.floor(Math.random() * 100);
+      this.reponse = "NON";
+      if (tirage >= proba) {
+        this.reponse = "OUI";
+      }
+      this.logTirage.unshift(
+        `proba: ${proba}, tirage: ${tirage}, reponse: ${this.reponse}`
+      );
+    },
+  },
 };
 </script>
 
---
+++ src/components/Boutton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-2 w-full md:w-1/3">
+    <button class="px-4 py-2 w-full bg-gray-100 rounded">
+      {{ btName }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["btName"],
+};
+</script>
+
+<style></style>
